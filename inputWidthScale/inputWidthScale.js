1315044035471073463539c82662534f2e4abf4d
Folger fan <folger.fan@gmain.com>
2017-04-12 11:12:15 +0800

    input宽度自动伸缩测试页面

--- inputWidthScale/inputWidthScale.js
+++ inputWidthScale/inputWidthScale.js
@@ -1,25 +1,24 @@
 /**
  * Created by folgerfan on 2016/5/25.
  */
-(function($){
+(function ($) {
     var $span = $('<span style="position:absolute;opacity:0;z-index:-1;"></span>');
-    $.fn.inputWidthScale = function(option){
-        option = option||{};
+    $.fn.inputWidthScale = function () {
         var $el = $(this);
-        var minWidth = option.minWidth||$el.width();
+        var minWidth = $el.width();
         $span.css({
-            'font-size':$el.css('font-size'),
-            'font-family':$el.css('font-family'),
-            'font-weight':$el.css('font-weight')
+            'font-size': $el.css('font-size'),
+            'font-family': $el.css('font-family'),
+            'font-weight': $el.css('font-weight')
         });
-        $el.on('input',function(){
+        $el.on('input', function () {
             var val = $el.val();
             $span.html(val).appendTo($('body'));
             var spW = $span.width();
-            var newW = spW+4;
-            $el.width(newW>minWidth?newW:minWidth);
+            var newW = spW + 4;
+            $el.width(newW > minWidth ? newW : minWidth);
             $span.remove();
         });
 
     }
-})(window.jQuery||window.Zepto);
+})(window.jQuery || window.Zepto);
